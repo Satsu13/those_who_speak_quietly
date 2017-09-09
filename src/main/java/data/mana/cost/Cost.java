19ee9ad4a7565bb2e845dca7f693dd6d38487771
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 19:50:06 -0400

    cost implementation

--- src/main/java/data/mana/cost/Cost.java
+++ src/main/java/data/mana/cost/Cost.java
@@ -1,4 +1,24 @@
 package data.mana.cost;
 
+import data.mana.unit.Mana;
+
 public class Cost {
+    private Mana color;
+    private int amount;
+
+    public Mana getColor() {
+        return color;
+    }
+
+    public void setColor(Mana color) {
+        this.color = color;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
