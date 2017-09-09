4556fb7eb7f688295461f5042ecf7055f1203593
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 19:45:29 -0400

    game implementation

--- src/main/java/data/game/Game.java
+++ src/main/java/data/game/Game.java
@@ -1,4 +1,27 @@
 package data.game;
 
+import data.game.board.Board;
+import data.player.Player;
+
+import java.util.List;
+
 public class Game {
+    private List<Player> players;
+    private Board board;
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 }
