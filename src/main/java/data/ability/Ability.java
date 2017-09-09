5f5353495ba6fbf9096fa7e26f77bb4cdbf484ea
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 17:11:38 -0400

    ability internals

--- src/main/java/data/ability/Ability.java
+++ src/main/java/data/ability/Ability.java
@@ -1,4 +1,34 @@
 package data.ability;
 
 public class Ability {
+    private boolean hasName;
+
+    private String name;
+
+    private String description;
+
+    public boolean isHasName() {
+        return hasName;
+    }
+
+    public void setHasName(boolean hasName) {
+        this.hasName = hasName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
