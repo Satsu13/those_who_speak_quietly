d61e4099c2d2741e492e87018c9e5f67e4dd988b
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 19:57:14 -0400

    player implementation

--- src/main/java/data/player/Player.java
+++ src/main/java/data/player/Player.java
@@ -1,4 +1,13 @@
 package data.player;
 
 public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
