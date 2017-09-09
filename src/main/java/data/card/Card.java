d532ca787cddaaea4d181824697dd7355f812e30
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 17:03:59 -0400

    initial card internals

--- src/main/java/data/card/Card.java
+++ src/main/java/data/card/Card.java
@@ -1,4 +1,26 @@
 package data.card;
 
+import data.ability.Ability;
+
+import java.util.List;
+
 public class Card {
+    private String name;
+    private List<Ability> abilities;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ability> getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(List<Ability> abilities) {
+        this.abilities = abilities;
+    }
 }
