5196a67df0e312e8a01c13fa1d073d43a22945c5
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:51:45 -0400

    permanent implementation

--- src/main/java/data/collection/card/permanent/Permanent.java
+++ src/main/java/data/collection/card/permanent/Permanent.java
@@ -3,4 +3,13 @@ package data.collection.card.permanent;
 import data.collection.card.Card;
 
 public class Permanent extends Card {
+    private boolean isCranked;
+
+    public boolean isCranked() {
+        return isCranked;
+    }
+
+    public void setCranked(boolean cranked) {
+        isCranked = cranked;
+    }
 }
