ca2244cbd237361833b84900c2ef13ce5a064fef
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:28:17 -0400

    deck implementation

--- src/main/java/data/collection/Deck.java
+++ src/main/java/data/collection/Deck.java
@@ -1,4 +1,27 @@
 package data.collection;
 
+import data.collection.card.Card;
+
+import java.util.List;
+
 public class Deck {
+    private String name;
+
+    private List<Card> cards;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
 }
