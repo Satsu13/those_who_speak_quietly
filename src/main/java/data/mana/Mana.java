72c44736cb5f6976b72a36d4a5b5b2d7ac4d7d9a
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:50:04 -0400

    changed mana name to type

--- src/main/java/data/mana/Mana.java
+++ src/main/java/data/mana/Mana.java
@@ -1,13 +1,13 @@
 package data.mana;
 
 public class Mana {
-    private String name;
+    private String type;
 
-    public String getName() {
-        return name;
+    public String getType() {
+        return type;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setType(String type) {
+        this.type = type;
     }
 }
