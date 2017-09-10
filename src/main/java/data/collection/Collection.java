75bf42f49fa1b3a7c6141af39ce06541020513c5
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:24:42 -0400

    collection implementation

--- src/main/java/data/collection/Collection.java
+++ src/main/java/data/collection/Collection.java
@@ -1,4 +1,27 @@
 package data.collection;
 
+import data.card.Card;
+
+import java.util.List;
+
 public class Collection {
+    private List<Card> cards;
+
+    private List<Deck> decks;
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public List<Deck> getDecks() {
+        return decks;
+    }
+
+    public void setDecks(List<Deck> decks) {
+        this.decks = decks;
+    }
 }
