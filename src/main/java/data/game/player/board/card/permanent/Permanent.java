6b532980797527bb901dd5c182f64d141a4a81c0
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-10 01:36:25 -0400

    permanent implementation

--- src/main/java/data/game/player/board/card/permanent/Permanent.java
+++ src/main/java/data/game/player/board/card/permanent/Permanent.java
@@ -1,4 +1,13 @@
 package data.game.player.board.card.permanent;
 
 public class Permanent {
+    private boolean isCranked;
+
+    public boolean isCranked() {
+        return isCranked;
+    }
+
+    public void setCranked(boolean cranked) {
+        isCranked = cranked;
+    }
 }
