7072319f0e064891ea7de557c953439ed96c40e2
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:14:55 -0400

    InGamePlayers have lives

--- src/main/java/data/game/player/InGamePlayer.java
+++ src/main/java/data/game/player/InGamePlayer.java
@@ -4,8 +4,10 @@ import data.player.Player;
 
 public class InGamePlayer extends Player {
     private Hand hand;
-    
+
     private Board board;
+
+    private int life;
 
     public Hand getHand() {
         return hand;
@@ -21,5 +23,13 @@ public class InGamePlayer extends Player {
 
     public void setBoard(Board board) {
         this.board = board;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
     }
 }
