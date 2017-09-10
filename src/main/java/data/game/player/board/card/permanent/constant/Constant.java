63963045f544818c42075e40363be35daaadc014
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-10 01:36:17 -0400

    fixed Constant import

--- src/main/java/data/game/player/board/card/permanent/constant/Constant.java
+++ src/main/java/data/game/player/board/card/permanent/constant/Constant.java
@@ -1,6 +1,6 @@
 package data.game.player.board.card.permanent.constant;
 
-import data.collection.card.permanent.Permanent;
+import data.game.player.board.card.permanent.Permanent;
 
 public class Constant extends Permanent {
 }
