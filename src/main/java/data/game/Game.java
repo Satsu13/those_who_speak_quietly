1684339ab8bd4dbd0049ece3b4882b96fd621a76
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:11:48 -0400

    removed board from game

--- src/main/java/data/game/Game.java
+++ src/main/java/data/game/Game.java
@@ -1,13 +1,11 @@
 package data.game;
 
-import data.game.board.Board;
 import data.game.player.InGamePlayer;
 
 import java.util.List;
 
 public class Game {
     private List<InGamePlayer> players;
-    private Board board;
 
     public List<InGamePlayer> getPlayers() {
         return players;
@@ -15,13 +13,5 @@ public class Game {
 
     public void setPlayers(List<InGamePlayer> players) {
         this.players = players;
-    }
-
-    public Board getBoard() {
-        return board;
-    }
-
-    public void setBoard(Board board) {
-        this.board = board;
     }
 }
