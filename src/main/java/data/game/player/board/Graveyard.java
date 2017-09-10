f14e83122834de15f0b31cca20c4c2ff5b20584f
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:44:25 -0400

    graveyard commit

--- src/main/java/data/game/player/board/Graveyard.java
+++ src/main/java/data/game/player/board/Graveyard.java
@@ -1,4 +1,17 @@
 package data.game.player.board;
 
+import data.collection.card.Card;
+
+import java.util.List;
+
 public class Graveyard {
+    private List<Card> cards;
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
 }
