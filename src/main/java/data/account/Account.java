78711d2a00ed39841ffb7e3416b297dc7d1f0522
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-10 13:38:15 -0400

    refactor player to account

--- src/main/java/data/account/Account.java
+++ src/main/java/data/account/Account.java
@@ -1,12 +1,12 @@
-package data.player;
+package data.account;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
-public class Player {
+public class Account {
     public final ObjectProperty<String> name;
 
-    public Player() {
+    public Account() {
         name = new SimpleObjectProperty<String>();
     }
 }
--- src/main/java/data/game/player/InGamePlayer.java
+++ src/main/java/data/game/player/InGamePlayer.java
@@ -1,11 +1,11 @@
 package data.game.player;
 
 import data.game.player.board.Board;
-import data.player.Player;
+import data.account.Account;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
-public class InGamePlayer extends Player {
+public class InGamePlayer extends Account {
     public final ObjectProperty<Board> board;
 
     public final ObjectProperty<Integer> life;
