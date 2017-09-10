01da3fbb110d361eb8247b13a7efbdf603f81b33
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:04:53 -0400

    permanents cost implementation

--- src/main/java/data/cost/PermanentsCost.java
+++ src/main/java/data/cost/PermanentsCost.java
@@ -1,4 +1,13 @@
 package data.cost;
 
 public class PermanentsCost extends Cost {
+    private boolean mustTurnSideways;
+
+    public boolean isMustTurnSideways() {
+        return mustTurnSideways;
+    }
+
+    public void setMustTurnSideways(boolean mustTurnSideways) {
+        this.mustTurnSideways = mustTurnSideways;
+    }
 }
