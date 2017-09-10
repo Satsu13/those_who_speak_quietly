63d4836a1396a30de7a16783ae7c62c10ecb3e06
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:20:21 -0400

    board implmementation

--- src/main/java/data/game/player/board/Board.java
+++ src/main/java/data/game/player/board/Board.java
@@ -1,4 +1,47 @@
 package data.game.player.board;
 
+import data.card.permanent.Permanent;
+
+import java.util.List;
+
 public class Board {
+    private Hand hand;
+
+    private Graveyard graveyard;
+
+    private InGameDeck deck;
+
+    private List<Permanent> permanents;
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Graveyard getGraveyard() {
+        return graveyard;
+    }
+
+    public void setGraveyard(Graveyard graveyard) {
+        this.graveyard = graveyard;
+    }
+
+    public InGameDeck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(InGameDeck deck) {
+        this.deck = deck;
+    }
+
+    public List<Permanent> getPermanents() {
+        return permanents;
+    }
+
+    public void setPermanents(List<Permanent> permanents) {
+        this.permanents = permanents;
+    }
 }
