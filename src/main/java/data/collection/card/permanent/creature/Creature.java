ab9eca588e49054e80c040ea191016cadf4d2c1c
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-09 20:54:36 -0400

    creatures have attack and health

--- src/main/java/data/collection/card/permanent/creature/Creature.java
+++ src/main/java/data/collection/card/permanent/creature/Creature.java
@@ -3,4 +3,23 @@ package data.collection.card.permanent.creature;
 import data.collection.card.permanent.Permanent;
 
 public class Creature extends Permanent {
+    private int attack;
+
+    private int health;
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
 }
