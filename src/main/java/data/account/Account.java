5a67ea1668c33407fd5ee8e46f359faa655148ca
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-11 07:40:34 -0400

    accounts have collections

--- src/main/java/data/account/Account.java
+++ src/main/java/data/account/Account.java
@@ -1,12 +1,16 @@
 package data.account;
 
+import data.collection.Collection;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class Account {
     public final ObjectProperty<String> name;
+    
+    public final ObjectProperty<Collection> collection;
 
     public Account() {
         name = new SimpleObjectProperty<String>();
+        collection = new SimpleObjectProperty<Collection>();
     }
 }
