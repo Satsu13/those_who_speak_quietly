562160c8347d5889cb3dc47869526fe7413c9ebb
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-11 21:15:43 -0400

    fixed indentation

--- src/main/java/data/ability/Ability.java
+++ src/main/java/data/ability/Ability.java
@@ -4,7 +4,7 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class Ability {
-   public final ObjectProperty<Boolean> hasName;
+    public final ObjectProperty<Boolean> hasName;
 
     public final ObjectProperty<String> name;
 
