88512071e5eb86fbd1505da2bb7a5cefc23c7cf6
Nicholas Restifo <nsrestifo@hotmail.com>
2017-09-11 20:27:01 -0400

    permanents now represent cards

--- src/main/java/data/game/player/board/Permanent.java
+++ src/main/java/data/game/player/board/Permanent.java
@@ -1,12 +1,19 @@
 package data.game.player.board;
 
+import data.collection.card.Card;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class Permanent {
+    public final ObjectProperty<Card> originalCard;
+
+    public final ObjectProperty<Card> activeCard;
+
     public final ObjectProperty<Boolean> isCranked;
 
     public Permanent() {
+        originalCard = new SimpleObjectProperty<Card>();
+        activeCard = new SimpleObjectProperty<Card>();
         isCranked = new SimpleObjectProperty<Boolean>();
     }
 }
